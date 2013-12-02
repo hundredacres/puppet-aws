ced05b66fe044a086469a8072b52a60f62a1f41c
Chad Metcalf <metcalfc@puppetlabs.com>
2013-12-01 17:48:49 -0800

    Refactor the facter ruby into a module.

--- lib/facter/aws_tags.rb
+++ lib/facter/aws_tags.rb
@@ -2,34 +2,42 @@ require 'rubygems'
 require 'aws-sdk'
 require "net/http"
 
-# Here we need to get server.id
-INSTANCE_HOST = '169.254.169.254'
-INSTANCE_ID_URL = '/latest/meta-data/instance-id'
-INSTANCE_REGION_URL = '/latest/meta-data/placement/availability-zone'
+module Facter::AWSTags
 
-httpcall = Net::HTTP.new(INSTANCE_HOST)
-resp, instance_id = httpcall.get2(INSTANCE_ID_URL)
-resp, region = httpcall.get2(INSTANCE_REGION_URL)
+  # Here we need to get server.id
+  INSTANCE_HOST = '169.254.169.254'
+  INSTANCE_ID_URL = '/latest/meta-data/instance-id'
+  INSTANCE_REGION_URL = '/latest/meta-data/placement/availability-zone'
 
-# Cut out availability zone marker.
-# For example if region == "us-east-1c" after cutting out it will be
-# "us-east-1"
+  def self.get_tags
 
-region = region[0..-2]
+    httpcall = Net::HTTP.new(INSTANCE_HOST)
+    resp, instance_id = httpcall.get2(INSTANCE_ID_URL)
+    resp, region = httpcall.get2(INSTANCE_REGION_URL)
 
-# First we configure AWS sdk from amazon, region is
-# required if your instances are in other zone than the
-# gem's default one (us-east-1).
+    # Cut out availability zone marker.
+    # For example if region == "us-east-1c" after cutting out it will be
+    # "us-east-1"
 
-AWS.config(
-    :credential_provider => AWS::Core::CredentialProviders::EC2Provider.new,
-    :region => region)
+    region = region[0..-2]
 
-tags = AWS.ec2.instances[instance_id].tags.to_h
+    # First we configure AWS sdk from amazon, region is
+    # required if your instances are in other zone than the
+    # gem's default one (us-east-1).
 
-# tags is a hash so create a fact for each
-tags.each_pair do | key, value |
-  symbol = "ec2_tag_#{key.gsub(/\-|\//, '_')}".to_sym
-  Facter.add(symbol) { setcode { value } }
+    AWS.config(
+        :credential_provider => AWS::Core::CredentialProviders::EC2Provider.new,
+        :region => region)
+
+    tags = AWS.ec2.instances[instance_id].tags.to_h
+
+    # tags is a hash so create a fact for each
+    tags.each_pair do | key, value |
+      symbol = "ec2_tag_#{key.gsub(/\-|\//, '_')}".to_sym
+      Facter.add(symbol) { setcode { value } }
+    end
+  end
+
 end
 
+Facter::AWSTags.get_tags
